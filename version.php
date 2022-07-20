23cf20fc252dbc37c5c58f288bdb1c751da4d2dc
Ghostthinker <67102645+Ghostthinker@users.noreply.github.com>
2022-07-20 12:40:21 +0200

    Update version.php

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * IVS filter version information
+ * ivs filter version information
  *
  * @package    filter_ivs
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
